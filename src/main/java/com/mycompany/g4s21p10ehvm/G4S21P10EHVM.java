8e4f6d01d3f901ac11b1517ad0387c8f04212cba
GAARA <GAARA@GAARA>
2023-05-11 22:23:45 -0600

    Alumo Victor Manuel Esparza Hernandez trabajo de tabla de datos

---
+++ src/main/java/com/mycompany/g4s21p10ehvm/G4S21P10EHVM.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.g4s21p10ehvm;
+
+/**
+ *
+ * @author GAARA
+ */
+public class G4S21P10EHVM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+          new JFPrincipal().setVisible(true);
+    }
+}
